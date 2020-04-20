6fbb6a02b7c4eed023724814dc8fd69683ed4574
pradeep amatya <pradeepbdramatya@gmail.com>
2020-04-20 21:34:12 +0545

    Initial commit

---
+++ src/com/pradeep/PositiveNegativeZero.java
@@ -0,0 +1,19 @@
+package com.pradeep;
+
+public class PositiveNegativeZero {
+    public static  void main (String [] args) {
+      checkNumber(5);
+      checkNumber(-1);
+      checkNumber(0);
+
+    }
+    public static void checkNumber (int number){
+        if (number>0) {
+            System.out.println("Positive");
+        } else if (number<0) {
+            System.out.println("Negative");
+         } else if (number ==0){
+            System.out.println("Zero");
+        }
+    }
+}
